36ba64e4de9149103b43aa99220ca27464e18c7c
Wasan Manocheep <wasan@busisoft.co.th>
2025-09-21 08:56:37 +0000

    ใช้ทุกอย่างเป็น defaul ของ firebase

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "BS Leave Portal",
+  title: "BS Portal",
   description: "Leave management portal for Busisoft",
 };
 
